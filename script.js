4933016301bd0b8d21fe4f1b137de198410b1f3e
Salgueiro0 <salgueirog01@gmail.com>
2026-05-19 10:37:41 -0300

    teste

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
+// Configuração do Supabase
+const supabaseUrl = 'https://byujwlsvmahgyyjcssne.supabase.co';
+const supabaseKey = 'sb_publishable_wkXJxy14f7R5M5VOHLl5hg_-eSL3Ecg';
+const _supabase = supabase.createClient(supabaseUrl, supabaseKey);
+
 function checkLogin() {
     const user = document.getElementById('username').value.toLowerCase();
     const pass = document.getElementById('password').value;
 
-    // Defina aqui o login e a senha desejados
+    // Seus dados de acesso
     if (user === "sapecuda" && pass === "candiru") { 
-        window.location.href = "home.html"; // Nome da página que abrirá depois
+        window.location.href = "home.html"; 
     } else {
-        alert("Ops! Tente novamente, meu bem. 💕");
+        alert("Quase! Tenta de novo, vida. 💕");
     }
 }
 
@@ -14,9 +19,9 @@ let hintCount = 0;
 function showHint() {
     const hintElement = document.getElementById('hint-text');
     const hints = [
-        "Dica 1: É como eu te chamo todos os dias...",
-        "Dica 2: Que animal você é?",
-        "Dica 3: Sério? Tá muito fácil! O login você sabe sap... e a senha outra dica - você não é um Guaiamu"
+        "Dica 1: É aquele apelido bobo que a gente usa...",
+        "Dica 2: A senha é aquele peixe que você não gosta (ou gosta? kkk).",
+        "Dica 3: Login: sapecuda | Senha: candiru (Ta muito fácil!)"
     ];
 
     if (hintCount < hints.length) {
